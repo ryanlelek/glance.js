f417614c69007bd83007d4038054f328395349c6
Ryan Lelek <iam@ryanlelek.com>
2013-08-03 05:36:29 -0400

    Adding eye favicon.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon('public/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
